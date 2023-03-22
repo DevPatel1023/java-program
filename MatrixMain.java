933f09f3be4167f1ea85913aad711a378eaf1b7e
Dev Patel <118754663+alexop01@users.noreply.github.com>
2023-03-22 10:32:44 +0530

    Create MatrixMain.java

---
+++ MatrixMain.java
@@ -0,0 +1,38 @@
+class Matrix{
+    int row,coloumb;
+    float mat[][];
+    
+    Matrix(int a[][]){
+        
+    }
+    Matrix(){
+        
+    }
+    Matrix (int r,int c){
+        
+    } 
+    void readMatrix(){
+        
+    }
+    float [][] transpose(){
+        
+    }
+    float [][] matrixMultiplication(Matrix secound){
+        
+    }
+    void displayMatrix(float [][]a){
+        
+    }
+    void displayMatrix(){
+        
+    }
+    float Maximum_of_Array(){
+        
+    }
+    float Average_of_Array(){
+        
+    }
+    
+    
+    
+}
